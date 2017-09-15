db1c912e54a3ec2be507eb69128b496c6f64f632
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-14 14:07:49 -1000

    getHead added

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,51 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
 
+var linkedList = {};
+linkedList = linkedListGenerator();
+
+
+
+var linkedListGenerator = function(){
+   function getHead(){
+    return null;
+  }
+
+  function getTail(){
+    // if Next points to null, return that key's value
+    if(next === null){
+      return;
+    }
+  }
+
+  function add(value){
+    // set old tail to point to Added Item, set Added Item to point to null
+  }
+
+  function remove(number){
+    // [0] -> [1] -> [2] -> [3] -> null
+    // function remove ([2])
+    // make [1] point to [3]
+    // [0] -> [1] -> [3] -> null
+  }
+
+  function get(number){
+
+  }
+
+  function insert(value, number){
+  }
+
+  return {
+    getTail: getTail,
+    getHead: getHead,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert
+  };
 }
+
+
+
